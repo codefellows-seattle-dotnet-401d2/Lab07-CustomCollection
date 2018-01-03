cacfe62776be4fc7fd8aa090465181d0319535fb
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-01-02 23:17:40 -0800

    Okay too tired, need a nap.

---
+++ ECommerceStore/Classes/SuperList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ECommerceStore.Interfaces;
+
+namespace ECommerceStore.Classes
+{
+    public class SuperList : System.Collections.Generic.List<String>, iSuper
+    {
+        public int Size { get = this  }
+
+        public int AtIndexOf(Item item)
+        {
+            for(int i = 0; i <= this.Size; i++)
+            {
+                if(item == this.)
+            }
+        }
+    }
+}
